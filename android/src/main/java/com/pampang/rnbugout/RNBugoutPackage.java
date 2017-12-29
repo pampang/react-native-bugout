3d460cf22c0fc33ef8d541cc8140dcfce826e0fb
龐校長 <fspwz@foxmail.com>
2017-12-29 18:05:03 +0800

    [android]适配 react-native 0.47 版本(Remove unused createJSModules calls)

--- android/src/main/java/com/pampang/rnbugout/RNBugoutPackage.java
+++ android/src/main/java/com/pampang/rnbugout/RNBugoutPackage.java
@@ -23,7 +23,7 @@ public class RNBugoutPackage implements ReactPackage {
         });
     }
 
-    @Override
+    // Deprecated from RN 0.47
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
